480cb572eaa583789372bc869df2330947337b7e
Miguel Morís Gómez <miguel.moris.gomez@gmail.com>
2026-05-24 14:25:03 +0200

    Time: 57 ms (90.09%), Space: 19.2 MB (87.5%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -3,6 +3,6 @@ class Solution:
         res = []
         for i in range(n):
             res.append(nums[i]) 
-            res.append(nums[i+n])
+            res.append(nums[i + n])
         return res
         
