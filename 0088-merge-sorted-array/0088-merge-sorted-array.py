01a3ec6d12af57da1c4705d92c928aceff5de716
Miguel Morís Gómez <miguel.moris.gomez@gmail.com>
2026-07-13 12:51:36 +0200

    Time: 0 ms (100%), Space: 19.3 MB (76.6%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -6,6 +6,7 @@ class Solution:
         p1 = m - 1
         p2 = n - 1
         p_write = m + n - 1
+        
         while p1 >= 0 and p2 >= 0:
             if nums2[p2] > nums1[p1]:
                 nums1[p_write] = nums2[p2]
