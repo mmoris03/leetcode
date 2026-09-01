674ac4b24e76082512292a9976219ab7e4b26555
Miguel Morís Gómez <miguel.moris.gomez@gmail.com>
2026-09-01 18:36:02 +0200

    Time: 15 ms (78.15%), Space: 20.1 MB (97.55%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,10 +1,14 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        n = len(nums)
-        if n == 1:
-            return True
-        jump_pos = n - 1
-        for i in range(n - 2, -1, -1):
-            if i + nums[i] >= jump_pos:
-                jump_pos = i
-        return jump_pos == 0
+        max_reach = 0
+
+        for i, jump in enumerate(nums):
+            if i > max_reach:
+                return False
+
+            max_reach = max(max_reach, i + jump)
+
+            if max_reach >= len(nums) - 1:
+                return True
+
+        return True
